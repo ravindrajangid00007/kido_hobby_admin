edfbca72f05dd4dbab7e6bef9dfee9cfdccf46a9
gautam bansal <bansalgautam21@gmail.com>
2021-07-15 12:48:43 +0530

    change in dashboard UI and login_required feature added for views

--- admin_panal/models/teachers.py
+++ admin_panal/models/teachers.py
@@ -1,6 +1,18 @@
 from ..models import db
 
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String, unique=True, nullable=False)
-#     email = db.Column(db.String, unique=True, nullable=False)
+
+
+class Teacher(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String, nullable=False)
+    lastName = db.Column(db.String)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    address = db.Column(db.String, nullable=False)
+    city = db.Column(db.String, nullable=False)
+    state = db.Column(db.String, nullable=False)
+    zip = db.Column(db.String, nullable=False)
+    subject = db.Column(db.String, nullable=False)
+    status = db.Column(db.String, nullable=False, default='Active')
+
